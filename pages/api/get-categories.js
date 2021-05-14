6097a5722f643380e9f32a1bbe508d90ea4e11be
Carlos <53526597+carlosgauci@users.noreply.github.com>
2021-05-14 15:59:16 +0200

    get contentful categories through next api

---
+++ pages/api/get-categories.js
@@ -0,0 +1,41 @@
+// Get category list for use in navigation since we cant getStaticProps in non-page components
+import axios from "axios";
+
+const contentfulQuery = async (query, variables) => {
+  const result = await axios({
+    url: `https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}`,
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${process.env.CONTENTFUL_ACCESS_TOKEN}`,
+    },
+    data: {
+      query,
+      variables,
+    },
+  });
+
+  return result.data;
+};
+
+const CATEGORY_QUERY = `
+     query myQuery {
+        sexEdCategoriesCollection {
+            items {
+                name
+                slug
+            }
+        }
+    }
+`;
+
+export default async (req, res) => {
+  const { data, errors } = await contentfulQuery(CATEGORY_QUERY);
+
+  if (errors) {
+    return res.status(500).json({
+      errors,
+    });
+  }
+
+  return res.status(200).json(data.sexEdCategoriesCollection.items);
+};
