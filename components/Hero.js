ae34ad9c486037ff1ee828950d9f6a6dc0c56960
Carlos <53526597+carlosgauci@users.noreply.github.com>
2021-05-14 11:29:52 +0200

    create hero section, add fonts & tailwind base

---
+++ components/Hero.js
@@ -0,0 +1,41 @@
+import Image from "next/image";
+
+export default function Hero() {
+  return (
+    <section className="relative h-72 md:h-hero pt-14 md:pt-20 ">
+      {/* Background image */}
+      <div className="absolute inset-x-0 bottom-0 top-14 md:top-20 overflow-hidden -z-1 bg-black">
+        <Image
+          src="/images/hero.jpg"
+          layout="fill"
+          objectFit="cover"
+          quality={40}
+          loading="eager"
+          priority={true}
+          className="opacity-70"
+          alt="Pro Gardening banner"
+        />
+      </div>
+
+      {/* Content */}
+      <div className="py-8 px-2 h-full flex flex-col items-center justify-center text-white">
+        {/* Title */}
+        <section className="flex flex-col items-center mb-6 md:mb-10 text-shadow">
+          <h1 className=" text-4xl md:text-7xl font-bold text-white">
+            Pro Gardening
+          </h1>
+          <p className="font-heading font-bold self-end transform -translate-x-12 mb-0">
+            by <span className="text-green-400">Farmer Joe</span>
+          </p>
+        </section>
+
+        {/* Text & Search */}
+        <section className="max-w-xs md:max-w-xl flex flex-col items-center">
+          <p className="text-center font-heading md:text-2xl text-shadow">
+            Browse over 100+ hours of content related to gardening & farming.
+          </p>
+        </section>
+      </div>
+    </section>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,33 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      height: (theme) => ({
+        hero: "35rem",
+        featured: "37rem",
+      }),
+
+      zIndex: {
+        "-1": "-1",
+      },
+
+      fontFamily: {
+        body: ["Montserrat"],
+        heading: ["Poppins"],
+      },
+    },
+
+    container: {
+      padding: {
+        DEFAULT: "1rem",
+      },
+
+      center: true,
+
+      screens: {
+        xl: "1800px",
+      },
+    },
   },
   variants: {
     extend: {},
